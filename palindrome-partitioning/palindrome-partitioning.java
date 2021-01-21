bf1749e018804735b87b34d0d815a4295dc62e7f
dineshreddy5220 <77144744+dineshreddy5220@users.noreply.github.com>
2021-01-20 17:04:43 -0800

    Time: 11 ms (28.53%), Space: 52.6 MB (76.40%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.java
+++ palindrome-partitioning/palindrome-partitioning.java
@@ -1,24 +1,45 @@
 class Solution {
     List<List<String>> output;
+    HashMap<String,Boolean> palindromes;
     public List<List<String>> partition(String s) {
         output=new ArrayList<>();
-        backtrack(s,0,new ArrayList<>());
+        palindromes=new HashMap<>();
+        if(s.length()==0){
+            return output;
+        }
+        recursion(s,new ArrayList<>(),0);
         return output;
     }
-    public void backtrack(String s, int start, ArrayList<String> list){
-        if(start>=s.length()){
-            output.add( new ArrayList<>(list));
+    public void recursion(String s, List<String> list, int start){
+        if(start==s.length()){
+            output.add(new ArrayList(list));
             return;
         }
         for(int i=start+1;i<=s.length();i++){
-            if(isPalindrome(s.substring(start,i))){
-                list.add(s.substring(start,i));
-                backtrack(s, i,list);
+            String subString=s.substring(start,i);
+            if(isPalindrome(subString)){
+                list.add(subString);
+                recursion(s,list,i);
                 list.remove(list.size()-1);
             }
         }
     }
     public boolean isPalindrome(String s){
-        return s.equals((new StringBuilder(s)).reverse().toString());
+        if(palindromes.containsKey(s)){
+            return palindromes.get(s);
+        }
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+           if(s.charAt(i)!=s.charAt(j)){
+               palindromes.put(s,false);
+               return false;
+           } 
+            i++;
+            j--;
+        }
+        palindromes.put(s,true);
+        return true;
     }
+    
 }
