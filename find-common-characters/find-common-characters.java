0adf9a0d92adfb88dfd37e2b10d31eab31f0ffeb
dineshreddy5220 <77144744+dineshreddy5220@users.noreply.github.com>
2021-01-28 20:13:23 -0800

    Time: 35 ms (5.28%), Space: 43.1 MB (5.89%) - LeetHub

--- find-common-characters/find-common-characters.java
+++ find-common-characters/find-common-characters.java
@@ -1,27 +1,30 @@
 class Solution {
     public List<String> commonChars(String[] A) {
         List<String> result=new ArrayList<>();
-        int[] map=new int[26];
+        HashMap<Character,Integer> map=new HashMap<>();
         for(char c:A[0].toCharArray()){
-            map[c-'a']++;
+            map.put(c,map.getOrDefault(c,0)+1);
         }
         for(int i=1;i<A.length;i++){
-            int[] map2=new int[26];
+            HashMap<Character,Integer> map2=new HashMap<>();
             for(char c:A[i].toCharArray()){
-                map2[c-'a']++;
+                map2.put(c,map2.getOrDefault(c,0)+1);
             }
-            for(int j=0;j<26;j++){
-                map[j]=Math.min(map[j],map2[j]);
+            HashMap<Character,Integer> map3=new HashMap<>();
+            for(char c:map.keySet()){
+                int count=Math.min(map.get(c),map2.getOrDefault(c,0));
+                if(count>0){
+                    map3.put(c,count);
+                }
             }
+            map=map3;
         }
-        for(int j=0;j<26;j++){
-            int count=map[j];
-            while(count>0){
-                char c='a';
-                c+=j;
-                result.add(String.valueOf(c));
-                count--;
-            }
+        for(char c:map.keySet()){
+                int count=map.get(c);
+                while(count>0){
+                    result.add(String.valueOf(c));
+                    count--;
+                }
         }
         return result;
     }
