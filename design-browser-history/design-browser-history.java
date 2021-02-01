f59f2f16883849509157c2d9a8a12f689a0a4ed3
dineshreddy5220 <77144744+dineshreddy5220@users.noreply.github.com>
2021-02-01 01:41:02 -0800

    Time: 49 ms (55.84%), Space: 47.4 MB (64.94%) - LeetHub

--- design-browser-history/design-browser-history.java
+++ design-browser-history/design-browser-history.java
@@ -1,39 +1,45 @@
 class BrowserHistory {
-    Node current;
-    public BrowserHistory(String homepage) {
-        current=new Node(homepage);
-    }
-    
-    public void visit(String url) {
-        Node next=new Node(url);
-        current.next=next;
-        next.prev=current;
-        current=next;
-    }
-    
-    public String back(int steps) {
-        while(current.prev!=null && steps>0){
-            current=current.prev;
-            steps--;
-        }
-        return current.url;
-    }
-    
-    public String forward(int steps) {
-        while(current.next!=null && steps>0){
-            current=current.next;
-            steps--;
-        }
-        return current.url;
-    }
+    Node current;
+    public BrowserHistory(String homepage) {
+        current=new Node(homepage);
+    }
+    
+    public void visit(String url) {
+        Node next=new Node(url);
+        current.next=next;
+        next.prev=current;
+        current=next;
+    }
+    
+    public String back(int steps) {
+        while(current.prev!=null && steps>0){
+            current=current.prev;
+            steps--;
+        }
+        return current.url;
+    }
+    
+    public String forward(int steps) {
+        while(current.next!=null && steps>0){
+            current=current.next;
+            steps--;
+        }
+        return current.url;
+    }
 }
 class Node{
-    String url;
-    Node prev;
-    Node next;
-    public Node(String u){
-        url=u;
-    }
+    String url;
+    Node prev;
+    Node next;
+    public Node(String u){
+        url=u;
+    }
 }
-​
+
 /**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
